e8b0955bf5ecd445f11fab018b94b7cc0b1baf6f
ADOwen <a.einhorn886@gmail.com>
2021-11-17 14:35:53 -0500

    day 2 hw

--- day1_hw.py
+++ day1_hw.py
@@ -11,8 +11,8 @@ for num in range(500):
 
 def get_prime(range_1, range_2):
     print(f"\nHere are all the prime numbers from {range_1} to {range_2}: ")
-    # or print(<hr style = "border: 1px dashed red">) ;-)
     print("=========================================="*3)
+    # <hr style = "border: 1px dashed red"> ;-)
     for num in range(range_1, range_2 + 1):
         if num == 2 or (num > 1 and num % 2 != 0):
             print(num, end=", ")
